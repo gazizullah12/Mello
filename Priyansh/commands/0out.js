8330da472b025e2b2ff8015a3a5af6f1c892b25a
gazizullah12 <gazizullah12@gmail.com>
2025-09-29 22:51:38 +0500

    Update 0out.js

--- Priyansh/commands/0out.js
+++ Priyansh/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐏𝐫𝐢𝐧𝐜𝐞 𝐃𝐢𝐥𝐬𝐡𝐚𝐝",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
